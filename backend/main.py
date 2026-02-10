a17e4238ad9e8cee047222e2f02c17d92c5a37b5
tilakgupta2005 <tilakgupta2005@gmail.com>
2026-02-10 13:30:04 +0530

    fixed file_path for deletion after process completion

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,8 @@ from backend.editing_engine.template3 import Temp3
 app = FastAPI()
 
 @app.post("/template1")
-def get_template1(url: str, session_id: str): 
+def get_template1(url: str, session_id: str):
+    file_path = rf'temp\{session_id}\combined_video_bgm.mp4' 
     print(f"Received request for Template 1 with URL: {url} and Session ID: {session_id}")
     try:
         video_path, file_path = Temp1(url,session_id)
@@ -21,10 +22,13 @@ def get_template1(url: str, session_id: str):
             shutil.rmtree(file_path)
         return f"output/{session_id}.mp4"
     except Exception as e:
+        if file_path.exists() and file_path.is_dir():
+            shutil.rmtree(file_path)
         print(f"Error processing Template 1: {e}")
 
 @app.post("/template2")
 def get_template2(url: str, session_id: str): 
+    file_path = rf'temp\{session_id}\combined_video_bgm.mp4'
     print(f"Received request for Template 2 with URL: {url} and Session ID: {session_id}")
     try:
         video_path, file_path = Temp2(url,session_id)
@@ -35,10 +39,13 @@ def get_template2(url: str, session_id: str):
             shutil.rmtree(file_path)
         return f"output/{session_id}.mp4"
     except Exception as e:
+        if file_path.exists() and file_path.is_dir():
+            shutil.rmtree(file_path)
         print(f"Error processing Template 2: {e}")
 
 @app.post("/template3")
 def get_template3(url: str, session_id: str): 
+    file_path = rf'temp\{session_id}\combined_video_bgm.mp4'
     print(f"Received request for Template 3 with URL: {url} and Session ID: {session_id}")
     try:
         video_path, file_path = Temp3(url,session_id)
@@ -49,4 +56,6 @@ def get_template3(url: str, session_id: str):
             shutil.rmtree(file_path)
         return f"output/{session_id}.mp4"
     except Exception as e:
+        if file_path.exists() and file_path.is_dir():
+            shutil.rmtree(file_path)
         print(f"Error processing Template 3: {e}")
